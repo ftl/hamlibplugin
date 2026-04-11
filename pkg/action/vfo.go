1ae6cad5f2c97a27c5a0fee4cdc6432b9e978be8
Florian Thienel <ftl@thecodingflow.com>
2026-04-11 10:16:40 +0200

    move VFO related actions into one file

--- pkg/action/vfo.go
+++ pkg/action/vfo.go
@@ -8,13 +8,64 @@ import (
 )
 
 const (
+	SelectVFOUUID    = "com.thecodingflow.hamlibplugin.selectvfo"
 	VFOOpUUID        = "com.thecodingflow.hamlibplugin.vfoop"
 	VFOOpEncoderUUID = "com.thecodingflow.hamlibplugin.vfoopencoder"
 )
 
 func init() {
+	Factories[SelectVFOUUID] = NewSelectVFO
 	Factories[VFOOpUUID] = NewVFOOp
 	Factories[VFOOpEncoderUUID] = NewVFOOpEncoder
+}
+
+type SelectVFO struct {
+	basicAction
+}
+
+func NewSelectVFO(context string, client RigClient, deck Deck) Action {
+	return &SelectVFO{
+		basicAction: basicAction{
+			context: context,
+			client:  client,
+			deck:    deck,
+		},
+	}
+}
+
+func (a *SelectVFO) parseSettings(settings map[string]any) hl.VFO {
+	vfo, ok := settings["vfo"].(string)
+	if !ok {
+		vfo = ""
+	}
+	return hl.VFO(vfo)
+}
+
+func (a *SelectVFO) DidReceiveSettings(payload *sdk.ReceivedEventPayload) error {
+	a.UpdateVisual(payload)
+	return nil
+}
+
+func (a *SelectVFO) UpdateVisual(payload *sdk.ReceivedEventPayload) error {
+	vfo := a.parseSettings(payload.Settings)
+	if vfo == "" {
+		vfo = "VFO"
+	}
+	a.deck.SetTitle(a.context, string(vfo), sdk.HardwareAndSoftware)
+	return nil
+}
+
+func (a *SelectVFO) KeyDown(payload *sdk.ReceivedEventPayload) error {
+	vfo := a.parseSettings(payload.Settings)
+	if vfo == "" {
+		return nil
+	}
+
+	err := a.client.SetVFO(vfo)
+	if err != nil {
+		log.Printf("[ERROR] select vfo: %v", err)
+	}
+	return nil
 }
 
 type VFOOperation struct {
